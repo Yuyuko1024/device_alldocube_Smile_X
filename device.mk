f3ae32db9aa60994d44eb5ef8703aa5062b69794
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-09-06 09:46:46 +0800

    Smile_X: Add soong namespace

--- device.mk
+++ device.mk
@@ -42,3 +42,7 @@ PRODUCT_PACKAGES += \
 # Screen density
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)

cac1d8612a2d0696bcce9f64ea1a488b37de350b
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-09-06 19:35:38 +0800

    T1028: Use tablet config instead phone config This is a TABLET

--- lineage_Smile_X.mk
+++ lineage_Smile_X.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/alldocube/Smile_X/device.mk)
 
 # Inherit some common Pixel-Experience stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Smile_X

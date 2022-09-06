b92d7006798d01f85b3398664636cc95f93a8591
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-09-06 16:10:39 +0800

    Smile_X: Add A/B postinstall config

--- device.mk
+++ device.mk
@@ -39,6 +39,19 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 
 TARGET_HAS_GENERIC_KERNEL_HEADERS := true
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1200
